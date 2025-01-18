3d6ba5988cc9b116cc7032d29b6e10cbd625a5c1
HAEUN LEE <78083098+devhaeun@users.noreply.github.com>
2025-01-18 12:18:36 +0900

    [level 1] Title: 폰켓몬, Time: 0.55 ms, Memory: 34.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,10 +1,6 @@
 function solution(nums) {
-    const n = Math.floor(nums.length/2);
-    const ans = [];
-    nums.map(i => {
-        if (!(ans.includes(i))) ans.push(i);
-    });
-    // console.log(ans)
-    if (ans.length<n) return ans.length
-    else return n
+    const n = nums.length/2;
+    const set = new Set(nums);
+    
+    return set.size>n ? n : set.size
 }
