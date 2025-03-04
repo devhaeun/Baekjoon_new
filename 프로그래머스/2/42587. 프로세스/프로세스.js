78eeed815f62a8ac811c0b0496be29de908c9267
HAEUN LEE <78083098+devhaeun@users.noreply.github.com>
2025-03-04 10:26:19 +0900

    [level 2] Title: 프로세스, Time: 0.37 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -0,0 +1,17 @@
+function solution(priorities, location) {
+    const idxs = Array(priorities.length).fill().map((v,i)=>i);
+    let maxVal = Math.max(...priorities);
+    let answer = 0;
+    
+    while (priorities.length !== 0) {
+        if (priorities[0] < maxVal) {
+            priorities.push(priorities.shift());
+            idxs.push(idxs.shift());
+        } else {
+            answer++;
+            priorities.shift();
+            if (idxs.shift() === location) return answer;
+        }
+        maxVal = Math.max(...priorities);
+    }
+}
