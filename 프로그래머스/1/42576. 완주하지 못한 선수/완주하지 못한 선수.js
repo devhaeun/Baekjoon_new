9f9f74f335c9e8ad12c9b6b3d2caa23026e39596
HAEUN LEE <78083098+devhaeun@users.noreply.github.com>
2025-03-09 20:38:45 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 96.35 ms, Memory: 52.8 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,23 +1,8 @@
 function solution(participant, completion) {
-    const myMap = new Map();
+    participant.sort();
+    completion.sort();
     
-    for (const p of participant) {
-        if (!myMap.get(p)) {
-            myMap.set(p, 1);
-        } else {
-            myMap.set(p, myMap.get(p)+1);
-        }
-    }
-    
-    for (const c of completion) {
-        if (myMap.get(c)) {
-            myMap.set(c, myMap.get(c)-1);
-        }
-    }
-    
-    for (const p of participant) {
-        if (myMap.get(p)>=1) {
-            return(p);
-        }
+    for (let i=0; i<participant.length; i++) {
+        if (participant[i]!==completion[i]) return participant[i];
     }
 }
