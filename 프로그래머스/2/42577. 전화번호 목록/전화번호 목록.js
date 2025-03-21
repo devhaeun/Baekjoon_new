67acd29d87b7afb6961183d5a7f08c6061966e19
HAEUN LEE <78083098+devhaeun@users.noreply.github.com>
2025-03-21 19:00:57 +0900

    [level 2] Title: 전화번호 목록, Time: 158.94 ms, Memory: 79.8 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,9 +1,8 @@
 function solution(phone_book) {
     phone_book.sort();
     for (let i=0; i<phone_book.length-1; i++) {
-        if (phone_book[i]===phone_book[i+1].slice(0,phone_book[i].length)) {
-            return false;
-        }
-    }
+        if (phone_book[i]===phone_book[i+1].slice(0,phone_book[i].length))
+            return false
+    };
     return true;
 }
