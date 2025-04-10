42816e0ef6ee60cf041004d55abc8684f120343c
HAEUN LEE <78083098+devhaeun@users.noreply.github.com>
2025-04-10 10:57:02 +0900

    [level 3] Title: 입국심사, Time: 613.54 ms, Memory: 13.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -0,0 +1,16 @@
+def solution(n, times):
+    start, end = 1, max(times)*n
+    answer = 0
+    while start<=end:
+        mid = (start+end)//2
+        people = 0
+        for time in times:
+            people += mid//time
+            if people>=n:
+                break
+        if people>=n:
+            answer = mid
+            end = mid-1
+        else:
+            start = mid+1
+    return answer
