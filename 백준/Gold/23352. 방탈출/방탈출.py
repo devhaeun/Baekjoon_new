da742632f292c06d5f59343205afc4dc4770a0c7
HAEUN LEE <78083098+devhaeun@users.noreply.github.com>
2025-04-16 00:25:23 +0900

    [Gold V] Title: 방탈출, Time: 2092 ms, Memory: 243572 KB -BaekjoonHub

---
+++ 백준/Gold/23352. 방탈출/방탈출.py
@@ -0,0 +1,37 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+n,m = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(n)]
+answers = []
+
+# 우상좌하
+dy = [0, 1, 0, -1]
+dx = [1, 0, -1, 0]
+
+def bfs(sy,sx):
+    q = deque()
+    q.append((sy, sx, 0))
+    visited = [[False for _ in range(m)] for _ in range(n)]
+    visited[sy][sx]=True
+    
+    while q:
+        y, x, cnt = q.popleft()
+        flag = False
+        for i in range(4):
+            ny, nx = y+dy[i], x+dx[i]
+            if 0<=ny<n and 0<=nx<m and grid[ny][nx]>0 and not visited[ny][nx]:
+                flag = True
+                visited[ny][nx]=True
+                q.append((ny, nx, cnt+1))
+        if not flag:
+            answers.append((cnt, grid[sy][sx]+grid[y][x]))
+    
+for i in range(n):
+    for j in range(m):
+        if grid[i][j]>0:
+            bfs(i,j)
+            
+answers.sort(key=lambda x:(x[0],x[1]), reverse=True)
+print(answers[0][1])
