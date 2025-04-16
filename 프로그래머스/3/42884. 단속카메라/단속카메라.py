7a34ca1aafa1df28801e69131a1f0ed463d09890
HAEUN LEE <78083098+devhaeun@users.noreply.github.com>
2025-04-17 01:31:15 +0900

    [level 3] Title: 단속카메라, Time: 3.21 ms, Memory: 9.57 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.py
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.py
@@ -1,11 +1,33 @@
 def solution(routes):
-    routes.sort(key=lambda x: x[1])
-    key = -30001
-    cnt = 0
+    routes.sort(key=lambda x:x[0])
+    # print(routes)
+    cnt = 1
+    # section 범위 내에 있으면 카메라 추가 설치 필요 X
+    section = [-30000, -30000]
     
-    for route in routes:
-        if route[0]>key:
+    for i in range(len(routes)-1):
+        
+        # 겹치지 않는 경우
+        if routes[i][1]<routes[i+1][0]:
             cnt+=1
-            key = route[1]
-            
+            # print('1번', routes[i+1], cnt, section)
+        
+        # 겹치는 경우
+        elif routes[i+1][0]<=routes[i][1]:
+            # routes[i]와 routes[i-1]이 겹치지 않은 경우
+            if section[1]<routes[i][0]:
+                section = [routes[i+1][0], min(routes[i][1], routes[i+1][1])]
+                # print('2번', routes[i+1], cnt, section)
+            else:
+                # routes[i+1]도 section에 포함되는 경우
+                if routes[i+1][0]<=section[1]:
+                    section = [routes[i+1][0], min(routes[i+1][1], section[1])]
+                    # print('3번', routes[i+1], cnt, section)
+                # routes[i+1]은 포함되지 않는 경우
+                else:
+                    cnt+=1
+                    section = [routes[i+1][0], routes[i+1][1]]
+                    # print('4번', routes[i+1], cnt, section)
+                    
     return cnt
+                
