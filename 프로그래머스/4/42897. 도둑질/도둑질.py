82c8ab577383c5220bc05c3534d77a78f617d737
HAEUN LEE <78083098+devhaeun@users.noreply.github.com>
2025-04-18 16:23:25 +0900

    [level 4] Title: 도둑질, Time: 540.86 ms, Memory: 92.7 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/도둑질.py
+++ 프로그래머스/4/42897. 도둑질/도둑질.py
@@ -1,13 +1,26 @@
 def solution(money):
-    dp1 = [0]*len(money)
-    dp1[0] = money[0]
-    dp1[1] = max(money[0], money[1])
-    for i in range(2, len(money)-1):
-        dp1[i] = max(dp1[i-1], dp1[i-2]+money[i])
+    # 마지막 집을 털지 않는 경우
+    gains1 = [0 for _ in range(len(money)-1)]
+    gains1[0] = money[0]
+    gains1[1] = money[0]
     
-    dp2 = [0]*len(money)
-    dp2[1] = money[1]
+    for i in range(2,len(money)-1):
+        # gains1[i] = max(gains1[:i-1]) + money[i]
+        if gains1[i-2]+money[i] < gains1[i-1]:
+            gains1[i] = gains1[i-1]
+        else:
+            gains1[i] = gains1[i-2]+money[i]
+    gain1 = gains1[-1]
+        
+    # 첫 번째 집을 털지 않는 경우
+    gains2 = [0 for _ in range(len(money))]
+    gains2[1] = money[1]
     for i in range(2, len(money)):
-        dp2[i] = max(dp2[i-1], dp2[i-2]+money[i])
+        # gains2[i] = max(gains2[:i-1]) + money[i]
+        if gains2[i-2]+money[i] < gains2[i-1]:
+            gains2[i] = gains2[i-1]
+        else:
+            gains2[i] = gains2[i-2]+money[i]
+    gain2 = gains2[-1]
     
-    return max(max(dp1), max(dp2))
+    return max(gain1, gain2)
