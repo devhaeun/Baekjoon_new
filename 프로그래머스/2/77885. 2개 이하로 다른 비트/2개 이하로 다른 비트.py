b209a772ef87a3fd0e20f9d2450812e08c351091
HAEUN LEE <78083098+devhaeun@users.noreply.github.com>
2025-04-20 01:40:31 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 183.99 ms, Memory: 23 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.py
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.py
@@ -1,17 +1,16 @@
 def solution(numbers):
     answer = []
-    for number in numbers:
-        n = format(number, 'b')
-        cnt = 0
-        for i in n[::-1]:
-            if i=='1':
-                cnt+=1
-            else:
-                break
-        if cnt<2:
-            answer.append(number+1)
-        else:
-            n = n[:-cnt-1]+'10'+'1'*(cnt-1)
-            n = int(n, 2)
-            answer.append(n)
+    
+    for num in numbers:
+        bnum = list('0'+bin(num)[2:])
+        idx = ''.join(bnum).rfind('0')
+        bnum[idx] = '1'
+        
+        if num%2==1:
+            bnum[idx+1] = '0'
+            
+        answer.append(int(''.join(bnum),2))
+        
+    return answer
+            
     return answer
