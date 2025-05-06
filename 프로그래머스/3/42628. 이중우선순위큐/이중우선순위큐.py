a5751d0e53f62b9e2d2c9a113e46e79d562fa7b4
HAEUN LEE <78083098+devhaeun@users.noreply.github.com>
2025-05-06 23:44:47 +0900

    [level 3] Title: 이중우선순위큐, Time: 74.14 ms, Memory: 18.9 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -1,19 +1,48 @@
 import heapq
 def solution(operations):
-    heap = []
-    for operation in operations:
-        op, n = operation.split()
-        if op=="I":
-            heapq.heappush(heap, int(n))
-        else:
-            if len(heap)==0:
-                continue
-            if n=="1":
-                heap.pop(heap.index(max(heap)))
-            else:
-                heapq.heappop(heap)
-        
-    if heap:
-        return [heap[heap.index(max(heap))], heap[0]]
-    else:
-        return [0,0]
+	answer = []
+	
+	min_heap = []
+	max_heap = []
+	num = {} # 숫자 개수 저장
+	
+	for oper in operations:
+		t,n= oper.split()
+		n = int(n)
+		
+		if t=="I":
+			heapq.heappush(min_heap, n)
+			heapq.heappush(max_heap, -n)
+			if num.get(n):
+				num[n]+=1
+			else:
+				num[n] = 1
+				
+		else:
+			if n==1:
+				if len(max_heap):
+					add = -heapq.heappop(max_heap)
+				else:
+					continue
+				while max_heap and num[add]<1:
+					add = -heapq.heappop(max_heap)
+				num[add] -= 1
+				
+			else:
+				if len(min_heap):
+					add = heapq.heappop(min_heap)
+				else:
+					continue
+				while min_heap and num[add]<1:
+					add = heapq.heappop(min_heap)
+				num[add] -= 1
+				
+	while min_heap and num[min_heap[0]]<1:
+		heapq.heappop(min_heap)
+	min = min_heap[0] if len(min_heap) else 0
+		
+	while max_heap and num[-max_heap[0]]<1:
+		heapq.heappop(max_heap)
+	max = -max_heap[0] if len(max_heap) else 0
+		
+	return [max, min]
