b033f6adb732c549faf96e1ebe6ae4d6c93b4200
HAEUN LEE <78083098+devhaeun@users.noreply.github.com>
2025-11-04 17:55:43 +0900

    [level 2] Title: 모음 사전, Time: 311.08 ms, Memory: 36.5 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.js
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.js
@@ -1,20 +1,22 @@
 function solution(word) {
     let count = 0;
     let flag = false;
-    let vowels = ["A", "E", "I", "O", "U"];
+    const vowels = ["A", "E", "I", "O", "U"];
     
     const dfs = (cur) => {
-        if (cur.length>5 || flag) return;
+        if (cur.length > 5 || flag) return;
+        
         if (cur===word) {
             flag = true;
             return;
-        };
-        count++;
-        
-        for (let i=0; i<vowels.length; i++) {
-            dfs(cur+vowels[i]);
         }
-    };
+        
+        count++;
+        for (let i=0; i<5; i++) {
+            dfs(cur + vowels[i]);
+        }
+    }
+    
     dfs("");
     return count;
 }
